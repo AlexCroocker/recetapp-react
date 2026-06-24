e1600bc0f96e0efbfe1937add8b8a86959ec217b
AlexCroocker <alex.croocker@inacapmail.cl>
2026-06-24 17:26:08 -0400

    Etapa 1

---
+++ recetapp-react/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import { recetas } from './data/recetas' 
+
+function App () {
+  console.log(recetas);
+  return (
+    <div>
+      <h1>Recetas</h1>
+    </div>
+  )
+}
