d2187669a87834fa28be20a2361fc14e882a1f2c
AlexCroocker <alex.croocker@inacapmail.cl>
2026-06-24 18:09:30 -0400

    etapa 3 - renderizado de la lista con key

--- recetapp-react/src/App.jsx
+++ recetapp-react/src/App.jsx
@@ -1,5 +1,6 @@
 import { recetas } from './data/recetas'
-import { RecetaCard } from './components/RecetaCard'
+import RecetaCard from './components/RecetaCard'
+import { ListasRecetas } from './components/ListasRecetas'
 import './App.css'
 
 function App() {
@@ -12,25 +13,7 @@ function App() {
         </p>
       </header>
 
-      <section
-        style={{
-          display: 'grid',
-          gap: '24px',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
-        }}
-      >
-        {recetas.map((receta) => (
-          <RecetaCard
-            key={receta.id}
-            nombre={receta.nombre}
-            origen={receta.origen}
-            porciones={receta.porciones}
-            categoria={receta.categoria}
-            descripcion={receta.descripcion}
-            ingredientes={receta.ingredientes}
-          />
-        ))}
-      </section>
+      <ListasRecetas recetas={recetas} />
     </main>
   )
 }
---
+++ recetapp-react/src/components/ListasRecetas.jsx
@@ -0,0 +1,27 @@
+import { RecetaCard } from "./RecetaCard";
+
+export function ListasRecetas({ recetas }) {
+  return (
+    <section
+      style={{
+        display: 'grid',
+        gap: '24px',
+        gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
+      }}
+    >
+      {recetas.map((receta) => (
+        <RecetaCard
+          key={receta.id}
+          nombre={receta.nombre}
+          origen={receta.origen}
+          porciones={receta.porciones}
+          categoria={receta.categoria}
+          descripcion={receta.descripcion}
+          ingredientes={receta.ingredientes}
+        />
+      ))}
+    </section>
+  );
+}
+
+export default ListasRecetas
